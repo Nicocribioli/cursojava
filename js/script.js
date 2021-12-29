3c5fcb8049e2ba9e60d122f8d1875644230af48b
nicocribioli <nicolas-cribioli@hotmail.com>
2021-12-29 17:02:04 -0300

    entrega clase 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
 let nombre = prompt("Ingrese su nombre");
 let apellido = prompt("Ingrese su apellido");
-let alerta = alert ("Bienvenido "+nombre+" "+apellido);
+alert ("Bienvenido "+nombre+" "+apellido);
