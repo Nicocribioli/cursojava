81f92cc8c6aa0fa174d4dc21e207a2d4e52cd6bd
nicocribioli <nicolas-cribioli@hotmail.com>
2022-01-10 14:25:25 -0300

    entrega clase 3

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* 
 let nombre = prompt("Ingrese su nombre");
 let apellido = prompt("Ingrese su apellido");
 alert ("Bienvenido "+nombre+" "+apellido);
@@ -17,4 +18,55 @@ if (edad < 18){
     alert("Sos joven "+nombre+" tenes tiempo para aprender a tocar "+preferencia)
 }else if (edad > 18){
     alert("Nunca es tarde para apreder a tocar "+preferencia+" "+nombre)
+} */
+
+let hechizo = 10;
+let espada = 15;
+let arco = 10;
+let vidaJugador = 95;
+let vidaJefe = 35;
+
+do{
+    let ataqueDeseado = prompt("Te encuentras en la guarida del jefe final con tu hechizo, espada y arco ¿Cual deseas utilizar?");
+    if(ataqueDeseado=="hechizo" || ataqueDeseado=="espada" || ataqueDeseado=="arco");
+
+
+    switch(ataqueDeseado){
+        case "hechizo":
+            vidaJefe-=hechizo;
+            alert("Tu hechizo hizo 10hp de daño.");
+            alert("El jefe responde con una bola de fuego.");
+            vidaJugador-=30;
+            break;
+
+        case "espada":
+            vidaJefe-=espada;
+            alert("Tu espada hizo 15hp de daño.");
+            alert("El jefe responde con una bola de fuego.");
+            vidaJugador-=30;
+            break;
+
+            case "arco":
+                vidaJefe-=arco;
+                alert("Tu arco hizo 10hp de daño.");
+                alert("El jefe responde con una bola de fuego.");
+                vidaJugador-=30;
+                break;
+
+        default:
+            alert(ataqueDeseado+" No es un ataque disponible")
+            break;
 }
+if((vidaJugador > 0) && (vidaJefe > 0)){
+    alert("La batalla continua");
+}
+
+}while((vidaJugador > 0) && (vidaJefe > 0));
+
+if(vidaJugador == 0){
+    alert("Haz perdido la pelea")
+
+}else{
+    alert("¡Haz vencido al jefe final!")
+}
+
