b669dc264f93574bae8325d2ff4ea2e31f009242
nicocribioli <nicolas-cribioli@hotmail.com>
2022-03-09 13:36:07 -0300

    entrega final

--- js/script.js
+++ js/script.js
@@ -224,11 +224,11 @@ function agregarItemCarrito(nombre,precio,imagen) {
     listaCarrito.innerHTML = contenidoCarrito;
     itemsCarrito.append(listaCarrito);
     listaCarrito.getElementsByClassName("eliminarItem")[0].addEventListener("click", eliminarItem);
-    sumarTotal(precioTotal, precioItem);
+    sumarTotal(precioTotal);
 }
 
 function sumarTotal(precioTotal){
-    document.getElementsByClassName("carritoTotal")[0].innerHTML = precioTotal
+    document.getElementsByClassName("carritoTotal")[0].innerHTML =   " TOTAL " + " $ "+ precioTotal
     console.log(precioTotal)
 }
 
